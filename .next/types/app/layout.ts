62b25d52963d7af899ecdc07f350e963daf6a01c
pongdang04 <pongdangdang419@gmail.com>
2025-05-14 19:08:18 +0900

    refactoring of function

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/jintaek/Documents/project/erp/front/src/app/layout.tsx
+// File: C:\Projects\ErpProj\front\src\app\layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
