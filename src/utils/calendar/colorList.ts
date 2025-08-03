aa33c792900713a8b1543db8bfbedb63d5ba2b8d
pongdang04 <pongdangdang419@gmail.com>
2025-08-03 11:43:59 +0900

    feat: 예약 페이지 예약 색상 고정

--- src/utils/calendar/colorList.ts
+++ src/utils/calendar/colorList.ts
@@ -1,14 +1,1 @@
-export const colorList = [
-  "#B4D89C",
-  "#FFCB9B",
-  "#9CD8CD",
-  "#A59CD8",
-  "#D89CB9",
-  "#F9EE74",
-  "#9EDCF3",
-  "#D39CD8",
-  "#A7FFDB",
-  "#FF9277",
-  "#9CBAD8",
-  "#F0FE73",
-];
+export const colorList = ["#77A461"];
